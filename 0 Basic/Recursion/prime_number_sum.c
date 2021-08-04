40d2a1b1c124d07e539c93323176fc0b128bb369
Pushp Raj <raj.pushpi14@gmail.com>
2021-08-04 15:42:31 +0530

    Sum of Natural Number

--- 0 Basic/Recursion/prime_number_sum.c
+++ 0 Basic/Recursion/prime_number_sum.c
@@ -1,5 +1,17 @@
-include<stdio.h>
+#include<stdio.h>
+int add(int num)
+{
+    if(num!=0)
+        return num + add(num-1);
+    else    
+        return num;
+}
 int main()
 {
-
+    int n;
+    printf("Enter the number : ");
+    scanf("%d",&n);
+    int result = add(n);
+    printf("Sum is : %d",result);
+    return 0;
 }
